f0d9c9e1082ae32784141b50e209e0fe6d3c212c
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-17 15:29:23 -0700

    Clean hash files, add source map, babel loader and asset resource loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import generateJoke from './generateJoke';
 import './styles/main.scss';
+import emoji from './assets/emoji.png';
+
+const emojiImg = document.getElementById('emojiImg');
+emojiImg.src = emoji;
 
 console.log(generateJoke());
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
   },
+  devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist'),
@@ -26,6 +29,15 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
     ],
   },
   plugins: [
